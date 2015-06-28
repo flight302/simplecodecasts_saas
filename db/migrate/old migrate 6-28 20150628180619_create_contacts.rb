47186a1dfadfd618ef251af3b0d2a96bdde73474
David Nowell <flight302@msn.com>
2015-06-28 20:02:45 +0000

    updated the migrate and contacts form

--- db/migrate/old migrate 6-28 20150628180619_create_contacts.rb
+++ db/migrate/old migrate 6-28 20150628180619_create_contacts.rb
@@ -1,9 +1,9 @@
 class CreateContacts < ActiveRecord::Migration
   def change
-    create_table :contacts do |t|
-      t.string :name
-      t.name :email
-      t.text :comments
+    create_table: contacts do |t|
+      t.string: name
+      t.name: email
+      t.text: comments
       
       t.timestamps
     end
