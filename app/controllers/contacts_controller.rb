c8bc821b2f147ab0a218ac16b215cc4edc5ce643
David Nowell <flight302@msn.com>
2015-06-30 16:19:11 +0000

    controller.rb

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,16 +10,14 @@ class ContactsController < ApplicationController
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = params[:contact][:comments]
-      
       ContactMailer.contact_email(name, email, body).deliver
-      flash[:success] = 'Message sucessfully sent.'
-      redirect_to new_contact_path
-    else
-      flash[:danger] = 'Error occured, message has not been sent.'
-      redirect_to new_contact_path
-    end
+    flash[:success] = 'Message sent.'
+    redirect_to new_contact_path
+  else
+    flash[:danger] = 'Error occured, message has not been sent.'
+    redirect_to new_contact_path
   end
-  
+end
   private
     def contact_params
       params.require(:contact).permit(:name, :email, :comments)
