dfb6e391d36116bf1a4ec3e96965d60261a622b2
David Nowell <flight302@msn.com>
2015-06-30 14:12:33 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class Contactmailer < ActionMailer::Base
+    default to: 'flight302@gmail.com'
+    
+    def contact_email(name, email, body)
+        @name = name
+        @email = email
+        @body = body
+        
+        mail(from: email, subject: 'Contact Form Message')
+    end
+end
