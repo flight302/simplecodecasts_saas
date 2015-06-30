c0187e51b5d17ddf8ef8b059d94f1f384a9b2919
David Nowell <flight302@msn.com>
2015-06-30 15:54:37 +0000

    update environment2

--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,13 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
-
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_setting = {
-    :address => 'smpt.sendgrid.net',
-    :port => '587',
-    :authentication => :plan,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :enable_startstls_auto => true
+ActionMailer::Base.smtp_settings = {
+  :address => 'smtp.sendgrid.net',
+  :port => '587',
+  :authentication => :plain,
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
+  :enable_startstls_auto => true
 }
-
-
-
-
